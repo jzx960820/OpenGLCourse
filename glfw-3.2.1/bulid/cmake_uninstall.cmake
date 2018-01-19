8f19b6add413202a5227f51691e5cad0a06df245
jzx960820 <35243655+jzx960820@users.noreply.github.com>
2018-01-19 14:04:06 +0800

    open15
    第十五天

---
+++ glfw-3.2.1/bulid/cmake_uninstall.cmake
@@ -0,0 +1,29 @@
+
+if (NOT EXISTS "H:/OpenGL/glfw-3.2.1/bulid/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: \"H:/OpenGL/glfw-3.2.1/bulid/install_manifest.txt\"")
+endif()
+
+file(READ "H:/OpenGL/glfw-3.2.1/bulid/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+
+foreach (file ${files})
+  message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
+  if (EXISTS "$ENV{DESTDIR}${file}")
+    exec_program("H:/C++/Cmake/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
+    endif()
+  elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
+    EXEC_PROGRAM("H:/C++/Cmake/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing symlink \"$ENV{DESTDIR}${file}\"")
+    endif()
+  else()
+    message(STATUS "File \"$ENV{DESTDIR}${file}\" does not exist.")
+  endif()
+endforeach()
+
